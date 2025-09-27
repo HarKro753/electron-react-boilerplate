6436961756f19e87df0e7866f6d9a60e5180da3b
Your Name <your.email@example.com>
2025-09-27 22:34:12 +0200

    eslint

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,27 +1,27 @@
 import './App.css';
 import AppBar from './AppBar';
-import { BesucherCard } from './BesucherCard';
+import BesucherCard from './BesucherCard';
 
 function App() {
   return (
     <div className="h-screen w-screen bg-gray-100 flex flex-col overflow-hidden">
       <AppBar />
       <div className="flex flex-row h-full w-full gap-4 overflow-hidden">
-        <div className="flex-[1] h-full flex-shrink-0 flex flex-col overflow-hidden">
+        <div className="w-[300px] h-full flex-shrink-0 flex flex-col overflow-hidden">
           <h3 className="ml-2 mt-4 text-sm text-black font-semibold flex-shrink-0">
             Besucherliste
           </h3>
-          <div className="flex-1 w-full bg-white rounded-sm mt-4 overflow-y-auto scrollbar-hide border-t border-r border-b">
-            <BesucherCard name={'Bernd'} date={'12.03.2006'}></BesucherCard>
-            <BesucherCard name={'Bruno'} date={'21.06.2021'}></BesucherCard>
-            <BesucherCard name={'Harro'} date={'19.05.2016'}></BesucherCard>
-            <BesucherCard name={'Lasse'} date={'12.03.2006'}></BesucherCard>
-            <BesucherCard name={'Emil'} date={'12.03.2006'}></BesucherCard>
-            <BesucherCard name={'Lorenz'} date={'12.03.2006'}></BesucherCard>
+          <div className="flex-1 w-full bg-white rounded-sm mt-2 overflow-y-auto scrollbar-hide border-t border-r border-b">
+            <BesucherCard name="Bernd" date="12.03.2006" />
+            <BesucherCard name="Bruno" date="21.06.2021" />
+            <BesucherCard name="Harro" date="19.05.2016" />
+            <BesucherCard name="Lasse" date="12.03.2006" />
+            <BesucherCard name="Emil" date="12.03.2006" />
+            <BesucherCard name="Lorenz" date="12.03.2006" />
           </div>
         </div>
-        <div className="flex-[4] h-full bg-yellow-500"></div>
-        <div className="flex-[2] h-full bg-blue-500"></div>
+        <div className="flex-[1] h-full bg-white rounded-sm border mt-4" />
+        <div className="flex-[3] h-full bg-blue-500" />
       </div>
     </div>
   );
--- src/renderer/AppBar.tsx
+++ src/renderer/AppBar.tsx
@@ -4,20 +4,16 @@ import AppBarItem from './AppBarItem';
 export default function AppBar() {
   return (
     <div className="bg-gray-50 border flex flex-row gap-8 items-center px-8 py-2">
-      <AppBarItem
-        icon={<FiHome size={40} />}
-        label="Home"
-        onClick={() => console.log('Home clicked')}
-      />
+      <AppBarItem icon={<FiHome size={40} />} label="Home" onClick={() => {}} />
       <AppBarItem
         icon={<FiUser size={40} />}
         label="Profile"
-        onClick={() => console.log('Profile clicked')}
+        onClick={() => {}}
       />
       <AppBarItem
         icon={<FiSettings size={40} />}
         label="Settings"
-        onClick={() => console.log('Settings clicked')}
+        onClick={() => {}}
       />
     </div>
   );
